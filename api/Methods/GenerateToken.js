aae33bdc4aad47283074335b56186a6ed49d2368
jvmartinsdaSilva <joao_vitor@JoaoVitor>
2023-07-21 20:47:37 -0300

    Bug corretion in authenticate

--- api/Methods/GenerateToken.js
+++ api/Methods/GenerateToken.js
@@ -1,13 +1,11 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
 
-const secreet = process.env.SECREET_TOKEN
-
 export const GenerateToken =  id => {
     try{
         const token =  jwt.sign({
             id,
-        }, secreet, {expiresIn: 10800})
+        }, id, {expiresIn: 10800})
         return token
     } catch(err){
         return err
--- api/routes/dashboard/CheckToken.js
+++ api/routes/dashboard/CheckToken.js
@@ -1,17 +1,15 @@
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
-const secreet = process.env.SECREET_TOKEN
-
-
 export const checkToken = (req, res, next) => {
+    const id = req.params.id
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(" ")[1]
 
     if(!token) return res.status(404).json({tokenDenied: true, msg: "Acesso negado"})
 
     try{
-        jwt.verify(token, secreet)
+        jwt.verify(token, id)
         next()
     } catch(err){
         return res.status(404).json({tokenDenied: true, msg: "Tolken invalido"})
