12dd8d80dbce6c4e47eb1f49a36516254deee254
jvmartinsdaSilva <joao_vitor@JoaoVitor>
2023-08-01 22:57:19 -0300

    Add new token

--- api/Methods/GenerateToken.js
+++ api/Methods/GenerateToken.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
 
+
 export const GenerateToken =  id => {
+    const secret = `${process.env.SECRET_TOKEN}${id}`
     try{
         const token =  jwt.sign({
             id,
-        }, id, {expiresIn: 10800})
+        }, secret, {expiresIn: 10800})
         return token
     } catch(err){
         return err
