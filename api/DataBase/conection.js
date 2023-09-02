c338d9581f242232126ce0d1fe1f84a8eb1fea90
jvmartinsdaSilva <jvmartins0855@gmail.com>
2023-09-02 13:34:08 -0300

    Add useNewUrlParser

--- api/DataBase/conection.js
+++ api/DataBase/conection.js
@@ -2,5 +2,5 @@ import 'dotenv/config'
 import {MongoClient} from "mongodb"
 
 const url = process.env.MONGO_URI
-export const client = new MongoClient(url)
+export const client = new MongoClient(url, { useNewUrlParser: true })
 
