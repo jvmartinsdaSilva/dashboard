fab03a707f14a65af0779586b31d60b13e740d5e
jvmartinsdaSilva <jvmartins0855@gmail.com>
2023-09-02 13:39:00 -0300

    Test

--- api/DataBase/conection.js
+++ api/DataBase/conection.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import {MongoClient} from "mongodb"
 
+//Conection in Mongo DataBase
+
 const url = process.env.MONGO_URI
 export const client = new MongoClient(url, { useNewUrlParser: true })
 
